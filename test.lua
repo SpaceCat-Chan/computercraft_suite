2561d07a3a90a1fa589018f2071bd4619187cceb
SpaceCat-Chan <49094338+SpaceCat-Chan@users.noreply.github.com>
2020-12-24 15:38:57 +0100

    a

---
+++ test.lua
@@ -0,0 +1,17 @@
+require("textutils")
+
+local ws,err = http.websocket("ws://25.67.108.123")
+if ws then
+	local auth_message = ws.receive()
+	print(auth_message)
+	local auth_json = unserializeJSON(auth_message)
+	local back = {
+		["request-id"] = auth_json["request-id"],
+		response = {}
+	}
+	local back_message = serializeJSON(back)
+	print(back_message)
+	ws.send(back_message)
+else
+	print(err)
+end
