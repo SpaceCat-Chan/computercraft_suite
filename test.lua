d1679b0419717c0e0b577f06a6f321b87d9f5a6b
SpaceCat-Chan <49094338+SpaceCat-Chan@users.noreply.github.com>
2020-12-24 15:52:45 +0100

    asdsa

--- test.lua
+++ test.lua
@@ -4,7 +4,8 @@ local ws,err = http.websocket("ws://25.67.108.123")
 if ws then
 	local auth_message = ws.receive()
 	print(auth_message)
-	local auth_json = unserializeJSON(auth_message)
+	local auth_json, err = unserializeJSON(auth_message)
+	print(err)
 	local back = {
 		["request-id"] = auth_json["request-id"],
 		response = {}
