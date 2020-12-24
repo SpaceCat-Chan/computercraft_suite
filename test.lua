5bf461f59cbc895801a7f5e366752309c5f1bf1b
SpaceCat-Chan <49094338+SpaceCat-Chan@users.noreply.github.com>
2020-12-24 16:08:29 +0100

    oops

--- test.lua
+++ test.lua
@@ -1,16 +1,16 @@
-require("JSON")
+local JSON = require("JSON")
 
 local ws,err = http.websocket("ws://25.67.108.123")
 if ws then
 	local auth_message = ws.receive()
 	print(auth_message)
-	local auth_json, err = unserializeJSON(auth_message)
+	local auth_json = JSON:decode(auth_message)
 	print(err)
 	local back = {
 		["request-id"] = auth_json["request-id"],
 		response = {}
 	}
-	local back_message = serializeJSON(back)
+	local back_message = JSON:encode(back)
 	print(back_message)
 	ws.send(back_message)
 else
